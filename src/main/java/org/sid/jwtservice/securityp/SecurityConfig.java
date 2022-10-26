a2a5b6521b64800ea233299e64bc5df77a47be41
Ahmed Chakib <schackib@gmail.com>
2022-10-26 22:48:40 +0100

    Add filter/JwtAuth & filter/JwtAuthorizationF & Auth0

--- src/main/java/org/sid/jwtservice/securityp/SecurityConfig.java
+++ src/main/java/org/sid/jwtservice/securityp/SecurityConfig.java
@@ -1,19 +1,26 @@
 package org.sid.jwtservice.securityp;
 
 import org.sid.jwtservice.securityp.entities.AppUser;
+import org.sid.jwtservice.securityp.filter.JwtAuth;
+import org.sid.jwtservice.securityp.filter.JwtAuthorizationF;
 import org.sid.jwtservice.securityp.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -42,8 +49,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.headers().frameOptions().disable();
-        http.formLogin();
+        http.authorizeRequests().antMatchers("/h2-console/**","/refreshToken/**","/login/**").permitAll();
+        //http.formLogin();
+        //http.authorizeRequests().antMatchers(HttpMethod.POST, "/users/**").hasAuthority("ADMIN");
+        //http.authorizeRequests().antMatchers(HttpMethod.GET, "/users/**").hasAuthority("USER");
+
         http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(new JwtAuth(authenticationManagerBean()));
+        http.addFilterBefore(new JwtAuthorizationF(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
--- src/main/java/org/sid/jwtservice/securityp/web/AccountRestController.java
+++ src/main/java/org/sid/jwtservice/securityp/web/AccountRestController.java
@@ -1,16 +1,28 @@
 package org.sid.jwtservice.securityp.web;
 
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import org.sid.jwtservice.securityp.entities.AppRole;
 import org.sid.jwtservice.securityp.entities.AppUser;
 import org.sid.jwtservice.securityp.service.AccountService;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(value = "/users")
 public class AccountRestController {
     private AccountService accountService;
 
@@ -19,24 +31,62 @@ public class AccountRestController {
         this.accountService = accountService;
     }
 
-    @GetMapping
+    @GetMapping(path="/users")
+    @PostAuthorize("hasAuthority('USER')")
     public List<AppUser> appUser(){
         return accountService.listUsers();
     }
 
-    @PostMapping
+    @PostMapping(path="/users")
+    @PostAuthorize("hasAuthority('ADMIN')")
     public AppUser saveUser(@RequestBody AppUser appUser){
         return accountService.addNewUser(appUser);
     }
 
     @PostMapping(path="/roles")
+    @PostAuthorize("hasAuthority('ADMIN')")
     public AppRole saveRole(@RequestBody AppRole appRole){
+
         return accountService.addNewRole(appRole);
     }
 
-    @PostMapping
+    @PostMapping(path="/addRoleToUser")
     public void addRoleToUser(@RequestBody RoleUserForm roleUserForm){
         accountService.addRoleToUser(roleUserForm.getUsername(), roleUserForm.getRoleName() );
+    }
+
+    @GetMapping(path="/refreshToken")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        String authToken=request.getHeader("Authorization");
+        if (authToken!=null&&authToken.startsWith("Bearer")){
+            try {
+                String jwt=authToken.substring(7);
+                Algorithm algorithm=Algorithm.HMAC256("ahmedChakib");
+                JWTVerifier jwtVerifier= JWT.require(algorithm).build();
+                DecodedJWT decodedJWT=jwtVerifier.verify(jwt);
+                String username=decodedJWT.getSubject();
+                AppUser appUser=accountService.loadUserByUsername(username);
+                String jwtAccessToken = JWT.create()
+                        .withSubject(appUser.getUsername())
+                        .withExpiresAt(new Date(System.currentTimeMillis()+5*60*1000))
+                        .withIssuer(request.getRequestURI().toString())
+                        .withClaim("roles",appUser.getAppRoles().stream().map(r->r.getRoleName()).collect(Collectors.toList()))
+                        .sign(algorithm);
+
+                Map<String,String> idToken=new HashMap<>();
+                idToken.put("access-token", jwtAccessToken);
+                idToken.put("refresh-token", jwt);
+                response.setContentType("/application/json");
+                new ObjectMapper( ).writeValue(response.getOutputStream(),idToken);
+
+            }
+            catch (Exception e){
+                throw e;
+
+            }
+        }else {
+            throw new RuntimeException("Refresh Token Required");
+        }
     }
 }
 
